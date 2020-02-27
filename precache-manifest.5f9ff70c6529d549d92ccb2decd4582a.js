f7e7c532f8e56874341766972f84206aef2c18bc
rushnightfir3 <ashyoda@gmail.com>
2020-02-27 09:07:58 -0500

    Updates

---
+++ precache-manifest.5f9ff70c6529d549d92ccb2decd4582a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854e935a4611ea78da0c296b74198a15",
+    "url": "/Portfolio-5/index.html"
+  },
+  {
+    "revision": "a322bbc482c25e537797",
+    "url": "/Portfolio-5/static/css/main.44c77651.chunk.css"
+  },
+  {
+    "revision": "8d4a41b12f1bf2880432",
+    "url": "/Portfolio-5/static/js/2.29f29a72.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Portfolio-5/static/js/2.29f29a72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a322bbc482c25e537797",
+    "url": "/Portfolio-5/static/js/main.e054c496.chunk.js"
+  },
+  {
+    "revision": "65b2197feaf1d7145a80",
+    "url": "/Portfolio-5/static/js/runtime-main.abb321ea.js"
+  },
+  {
+    "revision": "b2597c3f4d9a5622309c146dee2a509d",
+    "url": "/Portfolio-5/static/media/2.b2597c3f.svg"
+  },
+  {
+    "revision": "e91f6fcc5a063ca153549a2c79594806",
+    "url": "/Portfolio-5/static/media/Logo.e91f6fcc.png"
+  },
+  {
+    "revision": "0ff19ab47576e3559ee7f4826e439265",
+    "url": "/Portfolio-5/static/media/mobilehero.0ff19ab4.png"
+  },
+  {
+    "revision": "0016dcc7c1703e85e596350ebd46da76",
+    "url": "/Portfolio-5/static/media/pikachu.0016dcc7.png"
+  },
+  {
+    "revision": "1aef206c2ef7e8e7b9f02922d91b7ff8",
+    "url": "/Portfolio-5/static/media/pokeballs.1aef206c.png"
+  },
+  {
+    "revision": "de09a7aa95e5871523968ad4ff79b594",
+    "url": "/Portfolio-5/static/media/pokemonhero.de09a7aa.png"
+  },
+  {
+    "revision": "646ac3603f10e0196dc6dd6c7cade70a",
+    "url": "/Portfolio-5/static/media/trainer.646ac360.png"
+  }
+]);
