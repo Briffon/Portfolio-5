ba4e8c1310bf462b0a6346f0497f0b323f2c84f9
rushnightfir3 <ashyoda@gmail.com>
2020-02-28 10:43:07 -0500

    reset init modal

--- src/components/pages/Builder.js
+++ src/components/pages/Builder.js
@@ -11,7 +11,7 @@ import TempSelector from "../tempSelector/TempSelector";
 
 function Builder() {
   const [teamName, setTeamName] = useState("");
-  const [showInitModal, setInitModal] = useState("closed");
+  const [showInitModal, setInitModal] = useState("open");
   const [errorFields, setErrorFields] = useState([]);
   const [team, setTeam] = useState();
   const [count, setCount] = useState(team ? team.team.length : 0);
