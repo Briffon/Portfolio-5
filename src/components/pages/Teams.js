8778534872736685618a3be1d2fc1c69e9ca8f2f
rushnightfir3 <ashyoda@gmail.com>
2020-02-28 10:41:49 -0500

    firday changes

--- src/components/pages/Teams.js
+++ src/components/pages/Teams.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 function Teams() {
   const [teams, setTeams] = useState([]);
@@ -13,43 +14,94 @@ function Teams() {
 
   const editTeam = e => {
     e.preventDefault();
+    let team = e.target.dataset.team;
+
+    localStorage.setItem("tempTeam", team);
   };
 
   const deleteTeam = e => {
     e.preventDefault();
+
+    let team = e.target.dataset.team;
+    let teams = localStorage.getItem("teams");
+    let id = e.target.dataset.index;
+    if (teams.includes(team)) {
+      {
+        console.log(id);
+        // let parsedTeam = JSON.parse(team);
+        let parsedTeams = JSON.parse(teams);
+        parsedTeams.teams.splice(id, 1);
+        console.log(parsedTeams);
+        localStorage.setItem("teams", JSON.stringify(parsedTeams));
+
+        // console.log(parsedTeams);
+        // let id = parsedTeams.teams.indexOf(parsedTeam);
+        // console.log(id);
+      }
+    }
   };
   return (
     <div className="team-page-container">
       {console.log(teams)}
-      {teams && teams.teams !== []
-        ? teams.map((team, index) => {
-            return (
-              <div
-                className="team-container"
-                key={index}
-                data-pokemon={JSON.stringify(team)}
-              >
-                <h2>{team.team.name}</h2>
-                <div className="oof">
-                  <div className="team-display">
-                    {team.team.team.map((mon, index) => {
-                      console.log(mon);
-                      return (
-                        <div className="team-poke-container" key={index}>
-                          <img src={mon.img} alt={mon.pokemon.name} />
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <div className="team-buttons">
-                    <button onClick={editTeam}>Edit</button>
-                    <button onClick={deleteTeam}>Delete</button>
-                  </div>
+      {teams && teams.teams !== [] ? (
+        teams.map((team, index) => {
+          return (
+            <div
+              className="team-container"
+              key={index}
+              data-pokemon={JSON.stringify(team)}
+            >
+              <h2>{team.team.name}</h2>
+              <div className="oof">
+                <div className="team-display">
+                  {team.team.team.map((mon, index) => {
+                    console.log(mon);
+                    return (
+                      <div className="team-poke-container" key={index}>
+                        <img src={mon.img} alt={mon.pokemon.name} />
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="team-buttons">
+                  {/* <button
+                    data-index={index}
+                    data-team={JSON.stringify(team)}
+                    onClick={editTeam}
+                  >
+                    Edit
+                  </button> */}
+                  <NavLink
+                    to="/Builder"
+                    data-index={index}
+                    data-team={JSON.stringify(team)}
+                    onClick={editTeam}
+                  >
+                    Edit
+                  </NavLink>
+                  {/* <button
+                    data-index={index}
+                    data-team={JSON.stringify(team)}
+                    onClick={deleteTeam}
+                  >
+                    Delete
+                  </button> */}
+                  <NavLink
+                    data-index={index}
+                    data-team={JSON.stringify(team)}
+                    to="/Teams"
+                    onClick={deleteTeam}
+                  >
+                    Delete
+                  </NavLink>
                 </div>
               </div>
-            );
-          })
-        : console.log(teams)}
+            </div>
+          );
+        })
+      ) : (
+        <h2>No Teams</h2>
+      )}
     </div>
   );
 }
